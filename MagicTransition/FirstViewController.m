58694d8b7fa6961912224af94e13d45523025da3
Boyce <910976804@qq.com>
2015-05-04 22:33:55 +0800

    update Demo to show how to use magic transition in autolayout environment

--- MagicTransition/FirstViewController.m
+++ MagicTransition/FirstViewController.m
@@ -28,9 +28,11 @@
 - (IBAction)push:(id)sender {
     SecondViewController *secondVC = [[SecondViewController alloc] initWithNibName:@"SecondViewController" bundle:nil];
     
-    // preload views to the memory
+    // preload views to the memory and get thue frames
     [secondVC loadView];
+    [secondVC.view layoutIfNeeded];
     
+    //[secondVC updateViewConstraints];
     // setup fromviews array and toviews array
     NSArray *fromViews = [NSArray arrayWithObjects:self.imageView1, self.imageView2, self.label1, nil];
     NSArray *toViews = [NSArray arrayWithObjects:secondVC.imageView1, secondVC.imageView2, secondVC.label1, nil];
