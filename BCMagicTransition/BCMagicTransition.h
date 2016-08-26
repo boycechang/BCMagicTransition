9bf9e55e73c66c1cd1aabee5d38d426cda384cc0
Dmitry Klimkin <dmitry.klimkin@gmail.com>
2016-08-26 17:54:59 +0800

    Moved all animation parameters into properties

--- BCMagicTransition/BCMagicTransition.h
+++ BCMagicTransition/BCMagicTransition.h
@@ -11,12 +11,20 @@
 #import <Foundation/Foundation.h>
 #import <UIKit/UIKit.h>
 
+#define DEFAULT_TRANSITON_DURATION 0.3
+#define DEFAULT_TRANSITON_DELAY    0.0
+#define DEFAULT_TRANSITON_DAMPING  0.8
+#define DEFAULT_TRANSITON_VELOCITY 1.0
+
 @interface BCMagicTransition : NSObject <UIViewControllerAnimatedTransitioning>
 
 @property (nonatomic, assign) BOOL isPush;
 @property (nonatomic, assign) BOOL isMagic;
 
 @property (nonatomic, assign) NSTimeInterval duration;
+@property (nonatomic, assign) NSTimeInterval delay;
+@property (nonatomic, assign) CGFloat        damping;
+@property (nonatomic, assign) CGFloat        velocity;
 
 @property (nonatomic, strong) NSArray *fromViews;
 @property (nonatomic, strong) NSArray *toViews;
